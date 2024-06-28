84a246836e6b07fe16a48a39bd8b1626b965466f
NicolasDona <nicolas.donascimento@gmail.com>
2024-06-28 15:15:25 +0200

    Update

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="CV de développeur intégrateur web en ligne pour recrutement rapide.">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/cv/public/assets/css/style.css?v=0.0.53">
+    <script src="https://kit.fontawesome.com/a1eac4c766.js" crossorigin="anonymous"></script>
+    <title>CV de développeur intégrateur web</title>
+    
+</head>
+
+<body>
+    <header class="container-fluid">
+        <!-- Nav Bar -->
+        <div class="row">
+            <nav class="navbar navbar-expand-md navbar-dark my-navbar">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav fw-bold gap-3 me-5">
+                            <li class="nav-item d-flex justify-content-center">
+                                <a class="nav-link text-blue text-decoration-none hover-underline" aria-current="page" href="#accueil">Accueil</a>
+                            </li>
+                            <li class="nav-item d-flex justify-content-center">
+                                <a class="nav-link text-blue text-decoration-none hover-underline" href="#profil">Profil</a>
+                            </li>
+                            <li class="nav-item d-flex justify-content-center">
+                                <a class="nav-link text-blue text-decoration-none hover-underline" href="#competences">Compétences</a>
+                            </li>
+                            <li class="nav-item d-flex justify-content-center">
+                                <a class="nav-link text-blue text-decoration-none hover-underline" href="#portfolio">Portfolio</a>
+                            </li>
+                            <li class="nav-item d-flex justify-content-center">
+                                <a class="nav-link text-blue text-decoration-none hover-underline" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <!-- ACCUEIL -->
+        <section class="accueil my-background" id="accueil">
+            <div class="container-fluid">
+                <div class="row text-white p-5">
+                    <div class="col-12 text-center mt-5 mb-2">
+                        <h1>Nicolas Do Nascimento</h1>
+                    </div>
+                    <div class="col-12 text-center mb-2">
+                        <h4>Développeur Intégrateur</h4>
+                        <h4>Web / Mobile</h4>
+                        <!-- <h6>en construction...</h6> -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- PROFILS -->
+        <section class="profil" id="profil">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="col-12 mt-5 mb-5">
+                        <h1>A PROPOS DE MOI</h1>
+                    </div>
+                    <!-- Photo à gauche -->
+                    <div class="col-12 col-md-6 d-flex align-items-center justify-content-center mb-5">
+                        <img src="/cv/public/assets/img/photo.webp" class="img-fluid pro-portrait" alt="Une photo complète du développeur">
+                    </div>
+                    <!-- Texte à droite -->
+                    <div class="col-12 col-md-6 d-flex align-items-center justify-content-center desc-style mb-5 p-5">
+                        <p>Ma passion pour l'avancement des technologies novatrices est un trait distinctif de mon
+                            parcours professionnel. Mon enthousiasme se traduit par la création et l'introduction
+                            de perspectives innovantes. Mon expérience professionnelle est marquée par une collaboration
+                            étroite au sein d'équipes diverses, où j'ai assumé avec compétence des rôles allant de
+                            l'employé au manager. Cette polyvalence m'a permis de comprendre l'importance de
+                            l'excellence
+                            dans chaque fonction, visant toujours à maximiser les résultats pour nos clients. Grâce à ma
+                            formation spécialisée, je suis en mesure d'offrir une expertise approfondie en
+                            développement web, prête à enrichir votre entreprise avec des compétences techniques
+                            avancées
+                            et une vision stratégique.
+                            <br><br>j'ai acquis des compétences solides dans la méthodologie Agile
+                            qui me permettent de conduire efficacement les projets
+                            et de m'adapter rapidement aux changements. J'utilise régulièrement des outils tels que
+                            Trello pour le suivi des tâches, la gestion de projets et la planification stratégique.
+                            Ces compétences et ces outils me permettent d'améliorer la productivité et de garantir
+                            la réalisation des objectifs dans les délais impartis.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- COMPETENCES -->
+        <section class="competence my-background" id="competences">
+            <div class="container-fluid">
+                <div class="row text-center text-white">
+                    <div class="col-12 mt-5 mb-5">
+                        <h1>COMPETENCES</h1>
+                    </div>
+                    <!-- Gauche -->
+                    <div class="col-12 col-md d-flex flex-column justify-content-center align-items-center">
+                        <i class="fa-brands fa-html5 fa-2xl col-12 mb-5 mt-5 comp-ico"> HTML</i>
+                        <i class="fa-brands fa-css3-alt fa-2xl col-12 mb-5 mt-5 comp-ico"> CSS</i>
+                        <i class="fa-brands fa-bootstrap fa-2xl col-12 mb-5 mt-5 comp-ico"> Bootstrap</i>
+                        <i class="fa-brands fa-js fa-2xl mb-5 mt-5 comp-ico"> JavaScript</i>
+                        <i class="fa-brands fa-php fa-2xl col-12 mb-5 mt-5 comp-ico"> PHP</i>
+                        <i class="fa-solid fa-database fa-2xl col-12 mb-5 mt-5 comp-ico"> SQL</i>
+                        <i class="fa-brands fa-square-github fa-2xl col-12 mb-5 mt-5 comp-ico"> Github</i>
+                        <i class="fa-brands fa-figma col-12 fa-2xl mb-5 mt-5 comp-ico"> FIGMA</i>
+                    </div>
+                    <!-- Droite -->
+                    <div class="col-12 col-md-6">
+                        <img src="/cv/public/assets/img/cv.webp" alt="image de mon CV" class="img-fluid">
+                        <div class="col-12">
+                            <div class="col-12 mt-2 mb-1">
+                                OBTENIR UNE VERSION PDF DE MON CV
+                            </div>
+                            <div class="col-12 mb-5">
+                                <a href="/cv/public/assets/docs/CV_2024-06-28_NICOLAS_DO_NASCIMENTO.pdf" target="_blank" aria-label="Télécharger le CV en version PDF" class="btn btn-primary">Télécharger</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- PORTFOLIO -->
+        <section class="portfolio" id="portfolio">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="col-12 mt-5 mb-1">
+                        <h1>PORTFOLIO</h1>
+                    </div>
+                    <!-- Carrousel -->
+                    <div id="carouselExampleDark" class="carousel carousel-dark slide mb-3">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        </div>
+                        <!-- Voir solution pour les balises <a> qui engloble une <div> (pour les liens en mode mobile) -->
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <a href="https://psyom.eu" target="_blank" rel="noopener noreferrer" class="open-link"><img src="/cv/public/assets/img/proj1.webp" class="d-block w-50 mx-auto" alt="Wordpress avec Elementor">
+                                    <div class="carousel-caption d-block cdescript">
+                                        <h5>Wordpress avec Elementor</h5>
+                                        <p>Utilisation d'un thème builder.</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="carousel-item">
+                                <a href="https://psyom.eu/pendu" target="_blank" rel="noopener noreferrer" class="open-link"><img src="/cv/public/assets/img/proj2.webp" class="d-block w-50 mx-auto" alt="Jeu du pendu en JavaScript">
+                                    <div class="carousel-caption d-block cdescript">
+                                        <h5>Jeu du pendu en JavaScript</h5>
+                                        <p>Mise en œuvre des écouteurs/sélectionneurs d'éléments.</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="carousel-item">
+                                <a href="https://psyom.eu/calendrier" target="_blank" rel="noopener noreferrer" class="open-link"><img src="/cv/public/assets/img/proj3.webp" class="d-block w-50 mx-auto" alt="Calendrier dynamique en PHP">
+                                    <div class="carousel-caption d-block cdescript">
+                                        <h5>Calendrier dynamique en PHP</h5>
+                                        <p>Récupération de données back et boucles dynamiques.</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="carousel-item">
+                                <a href="https://psyom.eu/lb" target="_blank" rel="noopener noreferrer" class="open-link"><img src="/cv/public/assets/img/proj4.webp" class="d-block w-50 mx-auto" alt="Projet de Formation">
+                                    <div class="carousel-caption d-block cdescript">
+                                        <h5>Projet de Formation</h5>
+                                        <p>Utilisation de toutes les compétences.</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- CONTACT -->
+        <section class="contact my-background" id="contact">
+            <div class="container-fluid">
+                <div class="row text-center text-white">
+                    <div class="col-12 mt-5 mb-3">
+                        <h1>CONTACT</h1>
+                    </div>
+                    <div class="col-12">
+                        <div class="mb-3"><a href="tel:0614217799" class="text-decoration-none hover-underline text-white">+33 614
+                                217 799</a></div>
+                        <div class="mb-5"><a href="mailto:nicolas.donascimento@gmail.com" class="text-decoration-none hover-underline text-white">nicolas.donascimento@gmail.com</a>
+                        </div>
+                        <!-- social media icons -->
+                        <div class="mt-3 mb-1 social-icons">
+                            <a href="https://www.linkedin.com/in/nicolas-d-694a44245/" target="_blank" class="text-decoration-none hover-underline text-white"><i class="bi bi-linkedin comp-ico"></i></a>
+                            <a href="https://github.com/NicolasDona" target="_blank" class="text-decoration-none hover-underline text-white"><i class="bi bi-github comp-ico"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- FOOTER -->
+    <footer>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="/cv/public/assets/js/script.js?v=0.0.1"></script>
+</body>
+
+</html>
